1e93fd0412d099e4687d799f5df2661b7a12875a
albertoruibal <alberto@alonsoruibal.com>
2013-04-15 18:37:48 +0200

    setOnClickListener now works!

--- src/androidemu/widget/Button.java
+++ src/androidemu/widget/Button.java
@@ -1,8 +1,9 @@
 package androidemu.widget;
 
-import androidemu.content.DialogInterface.OnClickListener;
 import androidemu.view.View;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Element;
 
 public class Button extends View {
@@ -11,8 +12,14 @@ public class Button extends View {
 		super(element);
 	}
 
-	public void setOnClickListener(OnClickListener listener) {
-		// TODO
+	public void setOnClickListener(final OnClickListener listener) {
+		com.google.gwt.user.client.ui.Button.wrap(element).addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				listener.onClick(Button.this);
+			}
+		});
 	}
 
 }
