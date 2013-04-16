2f3c4ac50f86cfa4448b8b59b1bb8216f68381ea
albertoruibal <alberto@alonsoruibal.com>
2013-04-16 17:35:20 +0200

    CheckBox and RadioButton as childs of CompoundButton. Added CheckedChangeListener

--- src/androidemu/widget/CheckBox.java
+++ src/androidemu/widget/CheckBox.java
@@ -1,21 +1,11 @@
 package androidemu.widget;
 
-import androidemu.view.View;
-
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.InputElement;
 
-public class CheckBox extends View {
+public class CheckBox extends CompoundButton {
 
 	public CheckBox(Element element) {
 		super(element);
 	}
 
-	public boolean isChecked() {
-		return InputElement.as(element).isChecked();
-	}
-
-	public void setChecked(boolean checked) {
-		InputElement.as(element).setChecked(checked);
-	}
 }
--- src/androidemu/widget/RadioButton.java
+++ src/androidemu/widget/RadioButton.java
@@ -1,18 +1,11 @@
 package androidemu.widget;
 
-import androidemu.view.View;
-
 import com.google.gwt.dom.client.Element;
 
-public class RadioButton extends View {
+public class RadioButton extends CompoundButton {
 
 	public RadioButton(Element element) {
 		super(element);
-	}
-
-	public boolean isChecked() {
-		// TODO
-		return false;
 	}
 
 }
