dfb19e8d1ba2c26d274fd01363bebb865026482d
albertoruibal <alberto@alonsoruibal.com>
2013-04-24 17:09:54 +0200

    OnItemClickListener comments from AOSP

--- src/androidemu/widget/AdapterView.java
+++ src/androidemu/widget/AdapterView.java
@@ -6,7 +6,29 @@ public abstract class AdapterView<T extends Adapter> {
 
 	public static final int ITEM_VIEW_TYPE_IGNORE = -1;
 
+	/**
+	 * Interface definition for a callback to be invoked when an item in this
+	 * AdapterView has been clicked.
+	 */
 	public interface OnItemClickListener {
-		public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3);
+
+		/**
+		 * Callback method to be invoked when an item in this AdapterView has
+		 * been clicked.
+		 * <p>
+		 * Implementers can call getItemAtPosition(position) if they need to
+		 * access the data associated with the selected item.
+		 * 
+		 * @param parent
+		 *            The AdapterView where the click happened.
+		 * @param view
+		 *            The view within the AdapterView that was clicked (this
+		 *            will be a view provided by the adapter)
+		 * @param position
+		 *            The position of the view in the adapter.
+		 * @param id
+		 *            The row id of the item that was clicked.
+		 */
+		void onItemClick(AdapterView<?> parent, View view, int position, long id);
 	}
 }
