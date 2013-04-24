f7ea7775b8bc6907cc6d869e94514110aeb0ddd9
albertoruibal <alberto@alonsoruibal.com>
2013-04-24 17:01:42 +0200

    needed for Adapter

--- src/androidemu/widget/AdapterView.java
+++ src/androidemu/widget/AdapterView.java
@@ -2,7 +2,9 @@ package androidemu.widget;
 
 import androidemu.view.View;
 
-public class AdapterView<T extends Adapter> {
+public abstract class AdapterView<T extends Adapter> {
+
+	public static final int ITEM_VIEW_TYPE_IGNORE = -1;
 
 	public interface OnItemClickListener {
 		public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3);
