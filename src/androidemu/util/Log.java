ff20907248f0f705bb9c51b3ab9c53ce30735f3c
albertoruibal <alberto@alonsoruibal.com>
2013-04-26 19:45:49 +0200

    Emulate console error

--- src/androidemu/util/Log.java
+++ src/androidemu/util/Log.java
@@ -6,6 +6,10 @@ public class Log {
 	private static native void log(String message) /*-{
 		console.log(message);
 	}-*/;
+
+	private static native void error(String message) /*-{
+		console.error(message);
+	}-*/;
 	
 	public static void d(String tag, String message) {
 		log(tag + ": " + message);
@@ -16,6 +20,6 @@ public class Log {
 	}
 	
 	public static void e(String tag, String message) {
-		log(tag + ": " + message);
+		error(tag + ": " + message);
 	}
 }
