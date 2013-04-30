1586b13d0c701a6e2ded06ff059ef564eb2585d5
albertoruibal <alberto@alonsoruibal.com>
2013-04-30 11:30:08 +0200

    ScrollView methods implementation scrollTo(), getScrollX(), getScrollY()

--- src/androidemu/widget/ScrollView.java
+++ src/androidemu/widget/ScrollView.java
@@ -2,6 +2,7 @@ package androidemu.widget;
 
 import androidemu.view.View;
 
+import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Element;
 
 public class ScrollView extends View {
@@ -10,5 +11,16 @@ public class ScrollView extends View {
 		super(element);
 	}
 
+	public void scrollTo(int x, int y) {
+		DivElement.as(element).setScrollLeft(x);
+		DivElement.as(element).setScrollTop(y);
+	}
 
+	public int getScrollX() {
+		return DivElement.as(element).getScrollLeft();
+	}
+
+	public int getScrollY() {
+		return DivElement.as(element).getScrollTop();
+	}
 }
