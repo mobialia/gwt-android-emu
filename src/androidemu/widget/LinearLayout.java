fd4a160fd654c3604980f9ec4e29ba525a36c727
albertoruibal <alberto@alonsoruibal.com>
2013-05-07 13:30:34 +0200

    Linear layout stub

---
+++ src/androidemu/widget/LinearLayout.java
@@ -0,0 +1,13 @@
+package androidemu.widget;
+
+import androidemu.view.ViewGroup;
+
+import com.google.gwt.user.client.Element;
+
+public class LinearLayout extends ViewGroup {
+
+	public LinearLayout(Element element) {
+		super(element);
+	}
+
+}
