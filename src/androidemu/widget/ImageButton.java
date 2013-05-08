3892600b4d260dda1d95a3bcf0365ce5ab779098
albertoruibal <alberto@alonsoruibal.com>
2013-05-08 21:12:12 +0200

    ImageButton extends ImageView, so does not need to re-implement the setImageResource method

--- src/androidemu/widget/ImageButton.java
+++ src/androidemu/widget/ImageButton.java
@@ -2,14 +2,10 @@ package androidemu.widget;
 
 import com.google.gwt.dom.client.Element;
 
-public class ImageButton extends Button {
+public class ImageButton extends ImageView {
 
 	public ImageButton(Element element) {
 		super(element);
-	}
-
-	public void setImageResource(String image) {
-		element.setAttribute("src", image);
 	}
 
 }
