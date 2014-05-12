6776db786cf44eb61044f44a2b5bd0e645fc2431
albertoruibal <alberto@alonsoruibal.com>
2014-05-12 09:42:38 +0200

    Lostfragment working

--- androidemu/src/main/java/android/support/v4/app/ListFragment.java
+++ androidemu/src/main/java/android/support/v4/app/ListFragment.java
@@ -3,18 +3,30 @@ package android.support.v4.app;
 
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class ListFragment extends Fragment {
 
 	Adapter adapter;
+    ListView listView;
 
 	public void onListItemClick(ListView l, View v, int position, long id) {
 
 	}
 
-	public void setListAdapter(Adapter adapter) {
-		this.adapter = adapter;
+	public void setListAdapter(BaseAdapter adapter) {
+        this.adapter = adapter;
+        listView = ((ListView) mView.findViewById(android.R.id.list));
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                onListItemClick(listView, view, position, id);
+            }
+        });
+        adapter.notifyDataSetChanged();
 	}
 
 }
