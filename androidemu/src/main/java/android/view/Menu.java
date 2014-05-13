5117e260d20b116d7b24e0e257b6e98080222902
albertoruibal <alberto@alonsoruibal.com>
2014-05-13 11:27:50 +0200

    Add must return a menuitem

--- androidemu/src/main/java/android/view/Menu.java
+++ androidemu/src/main/java/android/view/Menu.java
@@ -6,13 +6,14 @@ public class Menu {
 
 	public ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
 
-	public void add(int groupId, int itemId, int order, String title) {
+	public MenuItem add(int groupId, int itemId, int order, String title) {
 		MenuItem item = new MenuItem();
 		item.setGroupId(groupId);
 		item.setItemId(itemId);
 		item.setOrder(order);
 		item.setTitle(title);
 		menuItems.add(item);
+        return item;
 	}
 
 }
