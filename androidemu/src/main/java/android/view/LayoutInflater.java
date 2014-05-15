d8456d52308e6bb3edd88c78ae66f70ae9f2edb5
albertoruibal <alberto@alonsoruibal.com>
2014-05-15 09:18:26 +0200

    Inflate also from Layout Resource ID

--- androidemu/src/main/java/android/view/LayoutInflater.java
+++ androidemu/src/main/java/android/view/LayoutInflater.java
@@ -1,5 +1,6 @@
 package android.view;
 
+import android.content.res.Resources;
 import com.google.gwt.resources.client.TextResource;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
@@ -16,12 +17,19 @@ public class LayoutInflater {
 		return new View((new HTML(layout.getText())).getElement());
 	}
 
-	public View inflate(Widget widget, Context ctx) {
-		return new View(widget);
+	public View inflate(int resId, Context ctx) {
+		return new View(Resources.getResourceResolver().getLayout(resId));
 	}
 
-	public View inflate(Widget widget, ViewGroup container, boolean something) {
-		return new View(widget);
+    public View inflate(Widget widget, Context ctx) {
+        return new View(widget);
+    }
+
+	public View inflate(int resId, ViewGroup container, boolean something) {
+		return new View(Resources.getResourceResolver().getLayout(resId));
 	}
 
+    public View inflate(Widget widget, ViewGroup container, boolean something) {
+        return new View(widget);
+    }
 }
