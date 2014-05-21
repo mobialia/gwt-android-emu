2f9ad24cdf3efb1b06f80b86b490251666578f85
albertoruibal <alberto@alonsoruibal.com>
2014-05-21 14:44:49 +0200

    Allow to create Button and ImageButton directly with new() Button setText() and getText

--- androidemu/src/main/java/android/widget/Button.java
+++ androidemu/src/main/java/android/widget/Button.java
@@ -1,13 +1,25 @@
 package android.widget;
 
+import android.content.res.Resources;
 import android.view.View;
-
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.DOM;
 
 public class Button extends View {
+
+	public Button() {
+		super(DOM.createButton());
+	}
 
 	public Button(Element element) {
 		super(element);
 	}
 
+	public void setText(int stringId) {
+		setText(Resources.getResourceResolver().getString(stringId));
+	}
+
+	public void setText(String string) {
+		element.setInnerHTML(string != null ? string.replace("\n", "<br/>") : "");
+	}
 }
--- androidemu/src/main/java/android/widget/ImageButton.java
+++ androidemu/src/main/java/android/widget/ImageButton.java
@@ -1,8 +1,14 @@
 package android.widget;
 
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.DOM;
 
 public class ImageButton extends ImageView {
+
+	public ImageButton() {
+		super(DOM.createElement("input"));
+		element.setAttribute("type", "image");
+	}
 
 	public ImageButton(Element element) {
 		super(element);
