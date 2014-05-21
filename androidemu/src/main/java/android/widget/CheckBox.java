7ab26c9168c12d42a22988760afc4c252268a701
albertoruibal <alberto@alonsoruibal.com>
2014-05-21 16:49:15 +0200

    Support ro create directy other android widgets as Checkbox and ImageView

--- androidemu/src/main/java/android/widget/CheckBox.java
+++ androidemu/src/main/java/android/widget/CheckBox.java
@@ -1,8 +1,13 @@
 package android.widget;
 
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.DOM;
 
 public class CheckBox extends CompoundButton {
+
+	public CheckBox() {
+		super(DOM.createInputCheck());
+	}
 
 	public CheckBox(Element element) {
 		super(element);
--- androidemu/src/main/java/android/widget/ImageView.java
+++ androidemu/src/main/java/android/widget/ImageView.java
@@ -3,8 +3,13 @@ package android.widget;
 import android.view.View;
 
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.DOM;
 
 public class ImageView extends View {
+
+	public ImageView() {
+		super(DOM.createImg());
+	}
 
 	public ImageView(Element element) {
 		super(element);
