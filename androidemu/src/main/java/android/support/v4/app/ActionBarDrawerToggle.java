efd3cc8d9c6182019d5c8d7a0cf49b430ef4dcde
albertoruibal <alberto@alonsoruibal.com>
2014-05-29 18:37:48 +0200

    Drawer layout simple implementation (without animation at the moment) TODO: start closed

--- androidemu/src/main/java/android/support/v4/app/ActionBarDrawerToggle.java
+++ androidemu/src/main/java/android/support/v4/app/ActionBarDrawerToggle.java
@@ -1,16 +1,39 @@
 package android.support.v4.app;
 
 
+import android.Res;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewFactory;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class ActionBarDrawerToggle implements DrawerLayout.DrawerListener {
 
-	public ActionBarDrawerToggle(Activity activity, DrawerLayout drawerLayout, int drawerImageRes, int openDrawerContentDescRes, int closeDrawerContentDescRes) {
+	static final String TAG = "ActionBarDrawerToggle";
 
+	DrawerLayout drawerLayout;
+	int drawerImageRes;
+	ImageView drawerImageView;
+
+	public ActionBarDrawerToggle(Activity activity, final DrawerLayout drawerLayout, int drawerImageRes, int openDrawerContentDescRes, int closeDrawerContentDescRes) {
+		this.drawerLayout = drawerLayout;
+		this.drawerImageRes = drawerImageRes;
+
+		drawerImageView = new ImageView(activity);
+		drawerImageView.setImageResource(drawerImageRes);
+		drawerImageView.element.addClassName(Res.R.style().actionbarDrawer());
+
+		LinearLayout actionBarLeft = new LinearLayout(ViewFactory.getElementById(activity.view.getElement(), "ActionBarLeft"));
+		if (actionBarLeft.getElement() == null) {
+			Log.e(TAG, "ActionBarLeft div not found");
+			return;
+		}
+		actionBarLeft.element.getParentElement().appendChild(drawerImageView.getElement());
 	}
 
 	public void onConfigurationChanged(Configuration newConfig) {
@@ -34,6 +57,14 @@ public class ActionBarDrawerToggle implements DrawerLayout.DrawerListener {
 	}
 
 	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			if (drawerLayout.isDrawerOpen(drawerLayout)) {
+				drawerLayout.closeDrawer(0);
+			} else {
+				drawerLayout.openDrawer(0);
+			}
+		}
+
 		return false;
 	}
 
--- androidemu/src/main/java/android/support/v4/widget/DrawerLayout.java
+++ androidemu/src/main/java/android/support/v4/widget/DrawerLayout.java
@@ -5,8 +5,9 @@ import android.view.ViewGroup;
 
 import com.google.gwt.dom.client.Element;
 
-
 public class DrawerLayout extends ViewGroup {
+
+	static final String TAG = "DrawerLayout";
 
 	public static interface DrawerListener {
 
@@ -19,20 +20,45 @@ public class DrawerLayout extends ViewGroup {
 		abstract void onDrawerStateChanged(int newState);
 	}
 
+	View drawer;
+	DrawerListener listener;
+	boolean isOpen;
+
 	public DrawerLayout(Element element) {
 		super(element);
+		int counter = 0;
+		for (int i = 0; i < element.getChildCount(); i++) {
+			if (element.getChild(i).getNodeName().equals("DIV")) {
+				if (++counter == 2) {
+					drawer = new View((Element) element.getChild(i));
+					break;
+				}
+			}
+		}
 	}
 
 	public void setDrawerListener(DrawerLayout.DrawerListener listener) {
+		this.listener = listener;
+	}
 
+	public boolean isDrawerOpen(View drawer) {
+		return isOpen;
 	}
 
 	public void openDrawer(int gravity) {
-
+		drawer.setVisibility(View.VISIBLE);
+		if (listener != null) {
+			listener.onDrawerOpened(drawer);
+		}
+		isOpen = true;
 	}
 
 	public void closeDrawer(int gravity) {
-
+		drawer.setVisibility(View.GONE);
+		if (listener != null) {
+			listener.onDrawerClosed(drawer);
+		}
+		isOpen = false;
 	}
 
 }
