5c1880a0c5d744ad616ed2d4f488c54fd0968c6a
albertoruibal <alberto@alonsoruibal.com>
2014-05-29 16:27:58 +0200

    Child views

--- androidemu/src/main/java/android/view/ViewGroup.java
+++ androidemu/src/main/java/android/view/ViewGroup.java
@@ -2,7 +2,13 @@ package android.view;
 
 import com.google.gwt.dom.client.Element;
 
+import java.util.ArrayList;
+
 public class ViewGroup extends View {
+
+	static final String TAG = "ViewGroup";
+
+	ArrayList<View> childViews = new ArrayList<View>();
 
 	public ViewGroup(Element element) {
 		super(element);
@@ -10,6 +16,15 @@ public class ViewGroup extends View {
 
 	public void addView(View v) {
 		element.appendChild(v.getElement());
+		childViews.add(v);
+	}
+
+	public View getChildAt(int index) {
+		return childViews.get(index);
+	}
+
+	public int getChildCount() {
+		return childViews.size();
 	}
 
 	public void removeAllViews() {
