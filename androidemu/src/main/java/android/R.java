d60da2b8df3a555f145a78ec4dbb23e91610efa8
albertoruibal <alberto@alonsoruibal.com>
2014-05-31 17:55:26 +0200

    Format

--- androidemu/src/main/java/android/R.java
+++ androidemu/src/main/java/android/R.java
@@ -9,6 +9,5 @@ public class R {
 	public static final class drawable {
 		public static final int actionbar_menu = 0x100001;
 		public static final int actionbar_indicator_back = 0x100002;
-
 	}
 }
--- androidemu/src/main/java/android/support/v4/app/Fragment.java
+++ androidemu/src/main/java/android/support/v4/app/Fragment.java
@@ -1,6 +1,5 @@
 package android.support.v4.app;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
