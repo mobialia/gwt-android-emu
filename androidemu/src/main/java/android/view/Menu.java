56b98173ff5343ef68c3f1e6439f18d730a4f3a7
albertoruibal <alberto@alonsoruibal.com>
2014-06-02 12:41:17 +0200

    implemented add(int) method

--- androidemu/src/main/java/android/view/Menu.java
+++ androidemu/src/main/java/android/view/Menu.java
@@ -6,6 +6,13 @@ public class Menu {
 
 	public ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
 
+	public MenuItem add(int title) {
+		MenuItem item = new MenuItem();
+		item.setTitle(title);
+		menuItems.add(item);
+		return item;
+	}
+
 	public MenuItem add(int groupId, int itemId, int order, int title) {
 		MenuItem item = new MenuItem();
 		item.setGroupId(groupId);
