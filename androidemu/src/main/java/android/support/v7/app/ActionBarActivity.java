f9d5aed660c5a77ddc8619cef5701b80d021f128
albertoruibal <alberto@alonsoruibal.com>
2015-04-22 16:16:47 +0200

    ActionBarActivity->AppCompatActivity

--- androidemu/src/main/java/android/support/v7/app/ActionBarActivity.java
+++ androidemu/src/main/java/android/support/v7/app/ActionBarActivity.java
@@ -1,32 +1,6 @@
 package android.support.v7.app;
 
-import android.support.v4.app.FragmentActivity;
-import android.widget.LinearLayout;
+@Deprecated
+public class ActionBarActivity extends AppCompatActivity {
 
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.Widget;
-
-public class ActionBarActivity extends FragmentActivity {
-
-	ActionBar actionBar;
-
-	public ActionBarActivity() {
-		actionBar = new ActionBar(this);
-	}
-
-	public ActionBar getSupportActionBar() {
-		return actionBar;
-	}
-
-	public void setContentView(Widget htmlPanel) {
-		String id = HTMLPanel.createUniqueId();
-		view = new LinearLayout(this);
-		view.addView(actionBar.view);
-
-		DOM.getElementById(ACTIVITY_ID).appendChild(view.getElement());
-		view.getElement().setId(id);
-		RootPanel.get(id).add(htmlPanel);
-	}
 }
---
+++ androidemu/src/main/java/android/support/v7/app/AppCompatActivity.java
@@ -0,0 +1,32 @@
+package android.support.v7.app;
+
+import android.support.v4.app.FragmentActivity;
+import android.widget.LinearLayout;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class AppCompatActivity extends FragmentActivity {
+
+	ActionBar actionBar;
+
+	public AppCompatActivity() {
+		actionBar = new ActionBar(this);
+	}
+
+	public ActionBar getSupportActionBar() {
+		return actionBar;
+	}
+
+	public void setContentView(Widget htmlPanel) {
+		String id = HTMLPanel.createUniqueId();
+		view = new LinearLayout(this);
+		view.addView(actionBar.view);
+
+		DOM.getElementById(ACTIVITY_ID).appendChild(view.getElement());
+		view.getElement().setId(id);
+		RootPanel.get(id).add(htmlPanel);
+	}
+}
