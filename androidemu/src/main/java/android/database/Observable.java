949a29353334c4b362480bc5314724d001bfe952
albertoruibal <alberto@alonsoruibal.com>
2015-11-19 19:36:58 +0100

    remove aosp comments

--- androidemu/src/main/java/android/database/Observable.java
+++ androidemu/src/main/java/android/database/Observable.java
@@ -1,41 +1,11 @@
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package android.database;
 
 import java.util.ArrayList;
 
-/**
- * Provides methods for (un)registering arbitrary observers in an ArrayList.
- */
 public abstract class Observable<T> {
-	/**
-	 * The list of observers. An observer can be in the list at most once and
-	 * will never be null.
-	 */
+
 	protected final ArrayList<T> mObservers = new ArrayList<T>();
 
-	/**
-	 * Adds an observer to the list. The observer cannot be null and it must not
-	 * already be registered.
-	 *
-	 * @param observer the observer to register
-	 * @throws IllegalArgumentException the observer is null
-	 * @throws IllegalStateException    the observer is already registered
-	 */
 	public void registerObserver(T observer) {
 		if (observer == null) {
 			throw new IllegalArgumentException("The observer is null.");
@@ -48,14 +18,6 @@ public abstract class Observable<T> {
 		}
 	}
 
-	/**
-	 * Removes a previously registered observer. The observer must not be null
-	 * and it must already have been registered.
-	 *
-	 * @param observer the observer to unregister
-	 * @throws IllegalArgumentException the observer is null
-	 * @throws IllegalStateException    the observer is not yet registered
-	 */
 	public void unregisterObserver(T observer) {
 		if (observer == null) {
 			throw new IllegalArgumentException("The observer is null.");
@@ -69,9 +31,6 @@ public abstract class Observable<T> {
 		}
 	}
 
-	/**
-	 * Remove all registered observer
-	 */
 	public void unregisterAll() {
 		synchronized (mObservers) {
 			mObservers.clear();
