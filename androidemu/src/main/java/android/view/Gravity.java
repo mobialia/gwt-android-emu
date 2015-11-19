99759c7090e138825359d8f779418f1cb38b22fc
albertoruibal <alberto@alonsoruibal.com>
2015-11-19 13:52:01 +0100

    Add NO_GRAVITY

--- androidemu/src/main/java/android/view/Gravity.java
+++ androidemu/src/main/java/android/view/Gravity.java
@@ -2,6 +2,7 @@ package android.view;
 
 public class Gravity {
 
+	public static final int NO_GRAVITY = 0x00000000;
 	public static final int TOP = 0x00000030;
 	public static final int CENTER = 0x00000011;
 	public static final int BOTTOM = 0x00000050;
